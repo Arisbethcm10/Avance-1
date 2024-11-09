60c308c4173113e8e38686e424be2ffd19bdbe1c
Arisbethcm10 <22690388@tecvalles.mx>
2024-11-08 21:59:59 -0600

    Dom.js

--- JS/Dom.js
+++ JS/Dom.js
@@ -15,3 +15,35 @@ parrafos2[1].innerHTML = "<strong>Nuevo-párrafo</strong>";
 let parrafo =
 document.querySelector(".parrafo");
 parrafo.className = "nuevo-parrafo";
+
+let parrafo_nuevo = document.createElement("p");
+parrafo_nuevo.innerText = "este parrafo tiene un emoji 😍";
+document.body.appendChild(parrafo_nuevo);
+console.log(parrafo_nuevo);
+
+
+
+
+let h1 = document.createElement("h1");
+h1.innerText= "Titulo dinamico";
+
+let hr = document.createElement("hr");
+
+parrafo_nuevo.prepend(h1);
+parrafo_nuevo.append(hr);
+
+
+
+
+//let parrafo = document.querySelector("p");
+let padre_parrafo = parrafo.parentElement;
+console.log(padre_parrafo); // body
+
+let lista = document.createElement("ol");
+for (let i = 1; i <= 5; i++) {
+let item = document.createElement("li");
+item.innerText = `Elemento ${i}`;
+lista.appendChild(item);
+}
+
+padre_parrafo.appendChild(lista);
